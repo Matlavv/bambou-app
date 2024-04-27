84e2d2d7ffe0d4702f42f284b9563e33eb1bc1d2
Matlavv <mathis.laversin92@gmail.com>
2024-04-27 15:31:15 +0200

    Logged gestion & home navigation

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Button, Text, View } from "react-native";
+import { AuthContext } from "../AuthContext";
+
+const HomeScreen = () => {
+  const { signOut } = useContext(AuthContext);
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>HomeScreen</Text>
+      <Button title="Déconnexion" onPress={() => signOut()} />
+    </View>
+  );
+};
+
+export default HomeScreen;

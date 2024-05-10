1e8c0e37efd13b84983a1a0212e6da542512636f
matlavv <mathis.laversin92@gmail.com>
2024-05-10 20:33:29 +0200

    events components

---
+++ components/EventsSection/AllEvents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AllEvents = () => {
+  return (
+    <View>
+      <Text>AllEvents</Text>
+    </View>
+  );
+};
+
+export default AllEvents;
---
+++ components/EventsSection/AllUpcomingEvents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AllUpcomingEvents = () => {
+  return (
+    <View>
+      <Text>All upcoming events</Text>
+    </View>
+  );
+};
+
+export default AllUpcomingEvents;
